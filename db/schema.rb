da2c7ad7285ca13c505bb43948f994dad81e843e
Fabricio Bezerra <demonstro003@gmail.com>
2017-11-14 13:05:18 -0200

    [skip ci] user updates profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113165438) do
+ActiveRecord::Schema.define(version: 20171114145915) do
 
   create_table "cuisines", force: :cascade do |t|
     t.string "name"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20171113165438) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "city"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
---
+++ spec/features/user_updates_profile_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature 'user updates profile' do
+  scenario 'sucessfully' do
+    user = create(:user, name: 'Christian', password: '123456')
+
+    login_as(user, scope: :user)
+    visit root_path
+    click_on 'Meu perfil'
+    click_on 'Atualizar meu perfil'
+
+    fill_in 'Nome', with: 'Ronaldo'
+    fill_in 'Cidade', with: 'São Paulo - SP'
+    fill_in 'Email', with: 'teste@teste.com'
+    fill_in 'Senha', with: '456123'
+    fill_in 'Confirmar senha', with: '456123'
+    fill_in 'Senha atual', with: '123456'
+    click_on 'Atualizar perfil'
+
+    expect(page).to have_css('.alert.alert-info',
+                             text: 'Sua conta foi atualizada com sucesso.')
+  end
+end
